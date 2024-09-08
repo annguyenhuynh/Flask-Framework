00d30f98a22d4507a8839cb7f935fb255592bc4e
annguyenhuynh <huynhleanh.270897@gmail.com>
2024-09-07 20:09:50 -0400

    upload new file

--- froshims/app.py
+++ froshims/app.py
@@ -1,3 +1,4 @@
+import flask
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
@@ -6,12 +7,15 @@ app = Flask(__name__)
 def index():
   return render_template('index.html')
 
-@app.route("/register",methods=["POST"])
+@app.route("/register", methods=["POST"])
 def register():
-  if not request.form.get("name") or not request.form.get("sport"):
+  name = request.form.get("name")
+  sport = request.form.get("sports")
+  print(f"Name: {name}, Sport: {sport}")  # Print to terminal for debugging
+    
+  if not name or sport=="":
     return render_template("failure.html")
-  return render_template("success.html")
+  return render_template('success.html')
   
-
 if __name__ == '__main__':
   app.run(debug=True)
